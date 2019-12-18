c6870bc9706acf4c1db03409956241a879ed7143
Marcin Kulig <marcin.kulig@ymail.com>
2019-12-18 23:38:35 +0100

    światło włącz/wyłącz

--- smartPi_source/functions/index.js
+++ smartPi_source/functions/index.js
@@ -2,11 +2,10 @@
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
 
-const util = require('util');
 const admin = require('firebase-admin');
 // Initialize Firebase
 
-const {dialogflow, BasicCard, Image} = require('actions-on-google');
+const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
 const app = dialogflow({debug: true});
 
@@ -14,8 +13,16 @@ const app = dialogflow({debug: true});
 admin.initializeApp();
 const firebaseRef = admin.database().ref('/');
 
+// zbiory  do rejestracji urzdzeń
+// const bathroom = new set('światło'); // setOfWords.has("zythum") => true/false
+// const livingroom = new set('światło');
+// const hall = new set('światło');
+// const bedroom = new set('światło');
+// const garage = new set('światło');
+// const kitchen = new set('światło');
+// const toilet = new set('światło');
 
-// eslint-disable-next-line max-len
+
 app.intent('garageDoorAction', (conv, {deviceGarageDoor, room, garageDoorAction}) => {
   const act = garageDoorAction;
   const ro = room;
@@ -23,17 +30,15 @@ app.intent('garageDoorAction', (conv, {deviceGarageDoor, room, garageDoorAction}
   return conv.ask('Robi się' + act + ro);
 });
 
-app.intent('lightAction', (conv, {room, generalAction}) => {
-  const act = generalAction;
-  const ro = room;
-  // var dev = device;
-  const params = true;
-  const ref = firebaseRef.child(ro).child(act);
-  const state = {OnOff: params};
+app.intent('lightAction', (conv, {actionLight, deviceLight, room}) => {
+  var action = actionLight + 'am';
+  var params = {'włącz': true, 'wyłącz': false};
+
+  const ref = firebaseRef.child(room).child('światło');
+  const state = {OnOff: params[actionLight]};
   ref.update(state)
       .then(() => state);
-
-  return conv.ask('Robi się' + act + ro);
+  return conv.ask(action + ' w pomieszczeniu ' + room);
 });
 
 
